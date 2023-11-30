98841fcc43e5a5ba920fe9af93a5b429838932e8
Roberto Proença <roberto@lumen.games>
2023-11-29 23:10:54 -0300

    Implement camera wrap

--- Assets/Scripts/Wrapper/WrapperController.cs
+++ Assets/Scripts/Wrapper/WrapperController.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using Extension;
 using UnityEngine;
 
 namespace Asteroidsberto.Wrapper
@@ -7,30 +6,24 @@ namespace Asteroidsberto.Wrapper
     public class WrapperSpaceController : MonoBehaviour
     {
         private readonly List<WrappedSpaceObject> _wrappedSpaceObjects = new();
-        private Camera _camera;
 
-        private void OnEnable()
-        {
-            _camera = Camera.main;
-        }
+        private const float AspectRatio = 16f / 9;
 
         private void FixedUpdate()
         {
-            Bounds cameraBounds = _camera.GetCameraBoundingBox();
-
             foreach (WrappedSpaceObject spaceObject in _wrappedSpaceObjects)
             {
-                if (spaceObject.Transform.position.x < cameraBounds.min.x)
-                    spaceObject.Transform.position += Vector3.right * cameraBounds.size.x;
-                if (spaceObject.Transform.position.x > cameraBounds.max.x)
-                    spaceObject.Transform.position += Vector3.left * cameraBounds.size.x;
-                if (spaceObject.Transform.position.y < cameraBounds.min.y)
-                    spaceObject.Transform.position += Vector3.up * cameraBounds.size.y;
-                if (spaceObject.Transform.position.y > cameraBounds.max.y)
-                    spaceObject.Transform.position += Vector3.down * cameraBounds.size.y;
+                if (spaceObject.Transform.position.x < -5 * AspectRatio)
+                    spaceObject.Transform.position += Vector3.right * (10 * AspectRatio);
+                if (spaceObject.Transform.position.x > 5 * AspectRatio)
+                    spaceObject.Transform.position += Vector3.left * (10 * AspectRatio);
+                if (spaceObject.Transform.position.y < -5)
+                    spaceObject.Transform.position += Vector3.up * 10;
+                if (spaceObject.Transform.position.y > 5)
+                    spaceObject.Transform.position += Vector3.down * 10;
             }
         }
-        
+
 
         public void RegisterObject(WrappedSpaceObject spaceObject)
         {
